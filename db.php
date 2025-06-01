8751b6fce3ae0dbb4b2e7aa40167b951f79451b5
hanifah <hanifahutamisunaryo@gmail.com>
2025-06-01 15:06:40 +0700

    databse baru

--- db.php
+++ db.php
@@ -1,21 +1,6 @@
-
 <?php
 $host = 'localhost'; // alamat server
 $dbname = 'qurbana_app';  // nama database
-$username = 'root';  // username mysql
-$password = '';      // password mysql
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-?>
-=======
-<?php
-$host = 'localhost'; // alamat server
-$dbname = 'qurban';  // nama database
 $username = 'root';  // username mysql
 $password = '';      // password mysql
 
